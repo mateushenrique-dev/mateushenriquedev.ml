7cef36669b84f9a0a6d7c4c1abf642fad1dbaa1c
Mateus Henrique <mhenriqq32@gmail.com>
2021-03-17 15:02:56 -0300

    update slide-right

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-const menuHamburguer = document.querySelector('#menu-hamburguer')
 
-function abrirMenu() {
-  const menuLista = document.querySelector('.menu ul')
-  menuLista.classList.toggle('ativo')
+function menuHamburguer() {
+  const menuHamburguer = document.querySelector('#menu-hamburguer')
+  function abrirMenu() {
+    const menuLista = document.querySelector('.menu ul')
+    menuLista.classList.toggle('ativo')
+  }
+  menuHamburguer.addEventListener('click', abrirMenu)
+  }
+menuHamburguer()
+
+function slideLeft() {
+  const sections = document.querySelectorAll('.js-slide');
+  const windowMetade = window.innerHeight * 0.6
+  function slide() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const isMetade = (sectionTop - windowMetade)
+    if (isMetade < 0) {
+      section.classList.add('slide')
+    } else {
+      section.classList.remove('slide')
+    }
+  })
+  }
+  window.addEventListener('scroll', slide)
 }
-
-menuHamburguer.addEventListener('click', abrirMenu)
+slideLeft()
