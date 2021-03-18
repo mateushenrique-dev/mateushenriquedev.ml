88193c238d0f1c63f25ab09a52054a2fa6199482
Mateus Henrique <mhenriqq32@gmail.com>
2021-03-18 18:35:01 -0300

    udate typing e menu

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ function slideLeft() {
   window.addEventListener('scroll', slide)
 }
 slideLeft()
+
+let i = 0;
+let message = 'Mateus Henrique Web Designer';
+
+digitando();
+
+function digitando() {
+  if(i < message.length) {
+    document.getElementById('text').innerHTML += message.charAt(i);
+    i++;
+    setTimeout(digitando, 150);
+  }
+}
+
